5b523c56c77ac671e53ed44d762e2460645d53ce
wafaasamir <wafaasamir1999@gmail.com>
2025-08-06 03:59:18 +0300

    fix image preview on update

--- src/app/(jihan)/profile/[id]/page.js
+++ src/app/(jihan)/profile/[id]/page.js
@@ -1,10 +1,12 @@
 "use client";
 import FreelancerProfile from "@/components/pages/FreelancerProfile/FreelancerProfile";
+import { useUserContext } from "@/context/userContext";
 import { getUser } from "@/services/userServices";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Profile() {
+  const { user: currentUser } = useUserContext();
   const params = useParams();
   const id = params.id;
   const [user, setUser] = useState();
@@ -13,5 +15,10 @@ export default function Profile() {
     getUser(id).then((res) => setUser(res));
   }, []);
 
-  return <FreelancerProfile user={user} refetchUser={() => getUser(id)} />;
+  return (
+    <FreelancerProfile
+      user={id!==currentUser?.id ? user : currentUser}
+      refetchUser={() => getUser(id)}
+    />
+  );
 }
