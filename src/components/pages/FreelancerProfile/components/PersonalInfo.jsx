033d073350f3975cad91d3a5bed90413982f0145
wafaasamir <wafaasamir1999@gmail.com>
2025-08-18 18:39:22 +0300

    handle bug of current job in profile

--- src/components/pages/FreelancerProfile/components/PersonalInfo.jsx
+++ src/components/pages/FreelancerProfile/components/PersonalInfo.jsx
@@ -174,7 +174,7 @@ export const PersonalInfo = ({
           <div className="text-center p-3 bg-slate-50 rounded-2xl border border-slate-200">
             <p className="text-sm text-slate-600 mb-1">Current Job</p>
             <p className="text-base font-semibold text-primary">
-              {typeof currentJob === "string" && currentJob
+              {(typeof currentJob === "string") && currentJob
                 ? currentJob
                 : "Available"}
             </p>
